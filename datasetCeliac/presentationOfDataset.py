4837726f7f179cdd7331d06d1447b770c76ed8db
JonasZerbib <50740546+JonasZerbib@users.noreply.github.com>
2020-11-24 21:51:47 +0200

    Update presentationOfDataset.py

--- datasetCeliac/presentationOfDataset.py
+++ datasetCeliac/presentationOfDataset.py
@@ -1,25 +1,9 @@
-import pickle
-from statistics import mean
-
-def recover_variable():
-    # Ouverture du fichier si il existe et récupération de la list
-    fichierSauvegarde = open("C:/Users/Jonas/PycharmProjects/ProjectLab/datasetCeliac/cleanData", "rb")
-    curve = pickle.load(fichierSauvegarde)
-    fichierSauvegarde.close()
-    return curve
-
-
 def display_metaData():
-    # recover_result
-    repertoires = recover_variable()
-    repertoire_len = []
-    for repertoire in repertoires:
-        repertoire_len.append(int(repertoire["size_patient"]))
-    repertoire_len.sort(reverse=True)
-    length = len(repertoire_len)
-    max = repertoire_len[0]
-    min = repertoire_len[len(repertoire_len)-1]
-    meann = int(mean(repertoire_len))
+    # recover results
+    length = 96
+    min = 2061
+    max = 54958
+    meann = 28014
     sick = 46
     unsick = 40
     numOfMEn = 18
